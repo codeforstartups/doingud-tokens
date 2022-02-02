d989da9fc49db1d830eb0f2a3936843ae05a8c3c
aguptaEtheralabs <agupta@etheralabs.io>
2022-02-02 23:01:17 +0530

    json creation for ECR1155 tokens

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -25,9 +25,34 @@ pragma solidity ^0.8.0;
  * so transfers to it are disabled to prevent tokens from being locked forever
  */
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract DGDTokenContract is ERC1155 {
-    uint256 public constant A = 0;
-    uint256 public constant B = 0;
-    uint256 public constant C = 0;
+    uint256 public constant AANG = 1;
+    uint256 public constant ZUKO = 2;
+    uint256 public constant KATARA = 3;
+    uint256 public constant AZULA = 4;
+    uint256 public constant BEIFONG = 5;
+    uint256 public constant SOKKA = 6;
+    uint256 public constant TYLEE = 7;
+    uint256 public constant APPA = 8;
+    uint256 public constant IROH = 9;
+    uint256 public constant OZAI = 10;
+    uint256 public constant SUKI = 11;
+    uint256 public constant PRINCES = 12;
+
+
+    constructor() public ERC1155("https://ipfs.io/ipfs/QmUV9LAbQnJwaoW2WUN9vAZo8riFWL26y33cNtmgNayAWC/{id}.json"){
+        _mint(msg.sender  , AANG , 100 , "");
+        _mint(msg.sender  , ZUKO , 100 , "");
+        _mint(msg.sender  , KATARA , 100 , "");
+        _mint(msg.sender  , AZULA , 100 , "");
+        _mint(msg.sender  , BEIFONG , 100 , "");
+        _mint(msg.sender  , SOKKA , 100 , "");
+        _mint(msg.sender  , TYLEE , 100 , "");
+        _mint(msg.sender  , APPA , 100 , "");
+        _mint(msg.sender  , OZAI , 100 , "");
+        _mint(msg.sender  , SUKI , 100 , "");
+        _mint(msg.sender  , PRINCES , 100 , "");
+    }
 }

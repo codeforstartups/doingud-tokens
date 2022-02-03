76916df6bb5587d13fd0bc28de459712e7fc3daa
aguptaEtheralabs <agupta@etheralabs.io>
2022-02-03 14:04:41 +0530

    improving minting functions while taking care about initialSupplies

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,7 +1,7 @@
 //Contracts/Token.sol
 //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
 /**
  * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-1155.md#erc-1155-metadata-uri-json-schema
@@ -26,38 +26,72 @@ pragma solidity ^0.8.0;
  */
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import 'multi-token-standard/contracts/tokens/ERC1155/ERC1155Metadata.sol';
+import 'multi-token-standard/contracts/tokens/ERC1155/ERC1155MintBurn.sol';
 
-contract DGDTokenContract is ERC1155 {
+contract OwnableDelegateProxy { }
+
+contract ProxyRegistry {
+  mapping(address => OwnableDelegateProxy) public proxies;
+}
+
+contract DGDTokenContract is ERC1155 , Ownable{
     uint256 public constant AANG = 1;
     uint256 public constant ZUKO = 2;
     uint256 public constant KATARA = 3;
     uint256 public constant AZULA = 4;
     uint256 public constant BEIFONG = 5;
-    uint256 public constant SOKKA = 6;
-    uint256 public constant TYLEE = 7;
-    uint256 public constant APPA = 8;
-    uint256 public constant IROH = 9;
-    uint256 public constant OZAI = 10;
-    uint256 public constant SUKI = 11;
-    uint256 public constant PRINCES = 12;
 
-    constructor()
+    address owner = 0x15977c6EA676299C697057b443eBff85cE0bF146;
+    address visitor = 0x63dA72e8D2C5847BEb823E758892855F3A604936;
+
+    uint256[] initialSupplies = [100,100,100,100,100];
+    uint256[] minted = [0,0,0,0,0];
+    
+    constructor(address owner_)
         public
         ERC1155(
             "https://ipfs.io/ipfs/QmUV9LAbQnJwaoW2WUN9vAZo8riFWL26y33cNtmgNayAWC/{id}.json"
         )
     {
-        _mint(msg.sender, AANG, 100, "");
-        _mint(msg.sender, ZUKO, 100, "");
-        _mint(msg.sender, KATARA, 100, "");
-        _mint(msg.sender, AZULA, 100, "");
-        _mint(msg.sender, BEIFONG, 100, "");
-        _mint(msg.sender, SOKKA, 100, "");
-        _mint(msg.sender, TYLEE, 100, "");
-        _mint(msg.sender, APPA, 100, "");
-        _mint(msg.sender, OZAI, 100, "");
-        _mint(msg.sender, SUKI, 100, "");
-        _mint(msg.sender, PRINCES, 100, "");
+
+        owner = owner_;
+       
+    }
+
+    modifier onlyOwner() {
+        require(owner == owner, "only owner can call this");
+        _;
+    }
+
+    function mint(address account, uint256 id, uint256 amount, byte memory data) public onlyOwner{
+
+
+        require(id <= initialSupplies.length, "Token does't exists");
+
+        require(id != 0, "Hooo, this we don't offer!");
+
+        uint256 index = id -1;
+
+        require(minted[index] + amount <= initialSupplies[index] , "For this token we don't have supplies");
+
+        _mint(account , id , amount , data);
+
+        minted[index] += amount; // after the minting is successful we will increase the amount for the supplies | reentrancy attack
+
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts , bytes memory data) public onlyOwner{
+        _mintBatch(to , ids , amounts , data);
+    }
+
+    function createERC1155Tokens(uint256 initialSupply) external onlyOwner{
+        mint(owner, AANG, initialSupply, "");
+        mint(owner, ZUKO, initialSupply, "");
+        mint(owner, KATARA, initialSupply, "");
+        mint(owner, AZULA, initialSupply, "");
+        mint(owner, BEIFONG, initialSupply, "");
     }
 
     function uri(uint256 _tokenId)
@@ -75,6 +109,22 @@ contract DGDTokenContract is ERC1155 {
                 )
             );
     }
+
+    /**
+        Batch Transfer: Transfer multiple assets in a single call.
+        Batch Balance: Get the balances of multiple assets in a single call.
+        Batch Approval: Approve all tokens to an address.
+        EIP-165 Support: Declare supported interfaces.
+        Hooks: Receive tokens hook.
+        NFT support: If supply is only 1, treat it as NFT.
+        Safe Transfer Rules: Set of rules for secure transfer.
+    */
+    
+    bool isAccessAllowed;
+
+    function userAccessNFTGating(uint _tokenId) public view returns(bool) {
+
+    }   
 
     
 }
